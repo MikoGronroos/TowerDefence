e75d9bc6ff9134811e8b9a2990811d297423a762
Miko Gronroos <47089401+MikoGronroos@users.noreply.github.com>
2021-12-19 03:45:28 +0200

    Added currency to the game to buy units and turrets

--- Assets/_PROJECT/Scripts/Shop/ShopItem.cs
+++ Assets/_PROJECT/Scripts/Shop/ShopItem.cs
@@ -6,8 +6,20 @@ public abstract class ShopItem : ScriptableObject
     public int Cost;
     public Sprite Icon;
 
-    public GameObject itemPrefab;
+    public GameObject ItemPrefab;
 
-    public abstract void Buy();
+    public void Buy()
+    {
+        if (VitrualCurrencyManager.Instance.CheckIfPlayerHasEnoughCurrency(Cost))
+        {
+            VitrualCurrencyManager.Instance.RemoveCurrency(Cost);
+            BuyAction();
+        }
+        else
+        {
+            return;
+        }
+    }
 
+    public abstract void BuyAction();
 }
