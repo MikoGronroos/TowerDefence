d78328e409c34e4aca0ec01adf6578ad361c1009
Miko Gronroos <47089401+MikoGronroos@users.noreply.github.com>
2021-12-19 20:51:35 +0200

    Added Income Increase From Unit Spawning

--- Assets/_PROJECT/Scripts/Shop/ShopItemUnit.cs
+++ Assets/_PROJECT/Scripts/Shop/ShopItemUnit.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Shop Item/Unit")]
 public class ShopItemUnit : ShopItem
 {
+
+    public int IncomeAddonFromSpawning;
+
     public override void BuyAction()
     {
+        VirtualCurrencyManager.Instance.AddIncome(IncomeAddonFromSpawning);
         UnitSpawner.Instance.SpawnUnit(ItemPrefab.name, PlayerManager.Instance.GetLocalPlayer().GetPlayerID());
     }
 }
