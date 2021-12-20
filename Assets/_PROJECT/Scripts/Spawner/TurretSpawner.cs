ccfcbe3ff0693fab843a0ee493ff06953b6edaad
Miko Gronroos <47089401+MikoGronroos@users.noreply.github.com>
2021-12-20 22:37:48 +0200

    Hauled Unit Spawn Networking

--- Assets/_PROJECT/Scripts/Spawner/TurretSpawner.cs
+++ Assets/_PROJECT/Scripts/Spawner/TurretSpawner.cs
@@ -4,25 +4,9 @@ using UnityEngine;
 public class TurretSpawner : MonoBehaviourSingleton<TurretSpawner>
 {
 
-    private PhotonView _photonView;
-
-    private void Awake()
-    {
-        _photonView = GetComponent<PhotonView>();
-    }
-
     public void SpawnTurret(string turretPrefabName)
     {
-        _photonView.RPC("RPCSpawnTurret", RpcTarget.MasterClient, turretPrefabName);
+        GameObject turret = PhotonNetwork.Instantiate(turretPrefabName, Vector2.zero, Quaternion.identity);
     }
 
-    //Spawns the object only on hosts client
-    [PunRPC]
-    private void RPCSpawnTurret(string turretPrefabName)
-    {
-        if (PhotonNetwork.IsMasterClient)
-        {
-            GameObject turret = PhotonNetwork.Instantiate(turretPrefabName, Vector2.zero, Quaternion.identity);
-        }
-    }
 }
