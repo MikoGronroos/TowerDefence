e785875c69c1fd21487ff30da238fd027ddf92ea
Miko Gronroos <47089401+MikoGronroos@users.noreply.github.com>
2022-01-31 20:37:57 +0200

    Added player account data class

--- Assets/_PROJECT/Scripts/PlayerAccount/Account.cs
+++ Assets/_PROJECT/Scripts/PlayerAccount/Account.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 public class Account : ScriptableObject
 {
 
+    public string AccountName;
+    public string AccountId;
+
+    public int AccountLevel;
+
     public int SoftCurrency;
     public int HardCurrency;
 
