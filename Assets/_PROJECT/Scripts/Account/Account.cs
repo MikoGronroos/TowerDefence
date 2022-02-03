ce021329c190bb9919121e49b3c6842f0de627cb
Miko Gronroos <47089401+MikoGronroos@users.noreply.github.com>
2022-02-03 23:20:10 +0200

    Added console commands to remove and add account level

--- Assets/_PROJECT/Scripts/Account/Account.cs
+++ Assets/_PROJECT/Scripts/Account/Account.cs
@@ -5,5 +5,6 @@ public class Account
     public string AccountName;
 
     public int AccountLevel;
+    public int AccountXp;
 
 }
---
+++ Assets/_PROJECT/Scripts/DeveloperCheats/ConsoleCommands/Commands/Account/AccountCmdRemoveLevel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ConsoleCommands/Account/RemoveLevel")]
+public class AccountCmdRemoveLevel : BaseCommand
+{
+    public override bool Process(string[] args)
+    {
+
+        int amount = Int32.Parse(args[0]);
+
+        string print = $"Removed {amount} Levels From Account Level.";
+
+        AccountManager.Instance.CurrentAccount.AccountLevel -= amount;
+
+        return true;
+    }
+}
