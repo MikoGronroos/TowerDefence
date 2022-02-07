f8ebad5d2b370a41522fdbe90718e89d6db73eab
Miko Gronroos <47089401+MikoGronroos@users.noreply.github.com>
2022-02-08 00:02:59 +0200

    Code refactore
    Made TurretJobAoe as base class where aoe turretjobs can derive

---
+++ Assets/_PROJECT/Scripts/Turret/TurretExecutable/Jobs/TurretJobAoe.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class TurretJobAoe : ExecuteJob
+{
+    public override void Job(Dictionary<string, object> args)
+    {
+
+        Debug.Log("Executed");
+        GameManager.Instance.StartChildCoroutine(MakeAoe(args));
+
+    }
+
+    private IEnumerator MakeAoe(Dictionary<string, object> args)
+    {
+
+        var projectileprefab = (GameObject)args["Prefab"];
+        var position = (Vector3)args["TargetPosition"];
+
+        var exec = (TurretExecutable)args["TurretExecutable"];
+
+        yield return new WaitForSeconds(1);
+
+        CollidersLogic(GetCollidersInArea(position, exec.Range.Value), exec);
+
+        GameObject clone = Instantiate(projectileprefab);
+        clone.transform.position = position;
+
+        FindObjectOfType<RangeVisualisation>().DrawCircle(clone, exec.Range.Value, 0.1f);
+
+    }
+
+    private Collider2D[] GetCollidersInArea(Vector3 position, float radious)
+    {
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(position, radious);
+
+        return colliders;
+
+    }
+
+    public virtual void CollidersLogic(Collider2D[] colliders, TurretExecutable exec)  {   }
+
+}
---
+++ Assets/_PROJECT/Scripts/Turret/TurretExecutable/Jobs/TurretJobAoeMortar.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Execute Jobs/Mortar")]
+public class TurretJobAoeMortar : TurretJobAoe
+{
+    public override void CollidersLogic(Collider2D[] colliders, TurretExecutable exec)
+    {
+        foreach (var collider in colliders)
+        {
+            if (collider.TryGetComponent(out IDamageable target))
+            {
+                target.Damage(exec.Damage.Value, exec.ProjectileTypes);
+            }
+        }
+    }
+}
