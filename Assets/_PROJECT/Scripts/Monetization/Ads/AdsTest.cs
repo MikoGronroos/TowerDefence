76a0e2f4e5d5fc8abc6c3723c3b5b0159dbb0d6b
Miko Gronroos <47089401+MikoGronroos@users.noreply.github.com>
2022-05-19 03:06:09 +0300

    Added ad playing

---
+++ Assets/_PROJECT/Scripts/Monetization/Ads/AdsTest.cs
@@ -0,0 +1,54 @@
+using UnityEngine.Advertisements;
+using UnityEngine;
+using System;
+
+public class AdsTest : MonoBehaviour, IUnityAdsLoadListener, IUnityAdsShowListener
+{
+    [SerializeField] string _androidAdUnitId = "Interstitial_Android";
+    [SerializeField] string _iOsAdUnitId = "Interstitial_iOS";
+    string _adUnitId;
+
+    void Awake()
+    {
+        // Get the Ad Unit ID for the current platform:
+        _adUnitId = (Application.platform == RuntimePlatform.IPhonePlayer)
+            ? _iOsAdUnitId
+            : _androidAdUnitId;
+    }
+
+    public void LoadAd(Action callback)
+    {
+        Advertisement.Load(_adUnitId, this);
+        callback?.Invoke();
+    }
+
+    public void ShowAd()
+    {
+        LoadAd(()=> {
+            Advertisement.Show(_adUnitId, this);
+        });
+        
+    }
+
+    // Implement Load Listener and Show Listener interface methods: 
+    public void OnUnityAdsAdLoaded(string adUnitId)
+    {
+        // Optionally execute code if the Ad Unit successfully loads content.
+    }
+
+    public void OnUnityAdsFailedToLoad(string adUnitId, UnityAdsLoadError error, string message)
+    {
+        Debug.Log($"Error loading Ad Unit: {adUnitId} - {error.ToString()} - {message}");
+        // Optionally execute code if the Ad Unit fails to load, such as attempting to try again.
+    }
+
+    public void OnUnityAdsShowFailure(string adUnitId, UnityAdsShowError error, string message)
+    {
+        Debug.Log($"Error showing Ad Unit {adUnitId}: {error.ToString()} - {message}");
+        // Optionally execute code if the Ad Unit fails to show, such as loading another ad.
+    }
+
+    public void OnUnityAdsShowStart(string adUnitId) { }
+    public void OnUnityAdsShowClick(string adUnitId) { }
+    public void OnUnityAdsShowComplete(string adUnitId, UnityAdsShowCompletionState showCompletionState) { }
+}
