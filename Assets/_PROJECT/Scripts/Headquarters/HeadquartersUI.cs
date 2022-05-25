b19bd5baa90544c5e3f3f27edd9d65ee6223b463
Miko Gronroos <47089401+MikoGronroos@users.noreply.github.com>
2022-05-26 01:15:59 +0300

    Modernized headquarters ui

--- Assets/_PROJECT/Scripts/Headquarters/HeadquartersUI.cs
+++ Assets/_PROJECT/Scripts/Headquarters/HeadquartersUI.cs
@@ -7,17 +7,17 @@ public class HeadquartersUI : MonoBehaviour
 	[SerializeField] private Button tabProfileButton;
 	[SerializeField] private Button tabInventoryButton;
 	[SerializeField] private Button tabStoreButton;
-    [SerializeField] private Button tabPlayButton;
 
     [SerializeField] private TabTween tabProfileTween;
     [SerializeField] private TabTween tabInventoryTween;
     [SerializeField] private TabTween tabStoreTween;
-    [SerializeField] private TabTween tabPlayTween;
 
     [SerializeField] private Canvas profileCanvas;
     [SerializeField] private Canvas inventoryCanvas;
     [SerializeField] private Canvas storeCanvas;
-    [SerializeField] private Canvas playCanvas;
+
+    [Header("Play")]
+    [SerializeField] private Button playButton;
 
     private bool _isTransistioning = false;
 
@@ -33,9 +33,7 @@ public class HeadquartersUI : MonoBehaviour
             profileCanvas.sortingOrder = 1;
             inventoryCanvas.sortingOrder = 0;
             storeCanvas.sortingOrder = 0;
-            playCanvas.sortingOrder = 0;
             tabProfileTween.Tween(()=> {
-                tabPlayTween.ResetTween();
                 tabInventoryTween.ResetTween();
                 tabStoreTween.ResetTween();
 
@@ -53,9 +51,7 @@ public class HeadquartersUI : MonoBehaviour
             profileCanvas.sortingOrder = 0;
             inventoryCanvas.sortingOrder = 1;
             storeCanvas.sortingOrder = 0;
-            playCanvas.sortingOrder = 0;
             tabInventoryTween.Tween(()=> {
-                tabPlayTween.ResetTween();
                 tabStoreTween.ResetTween();
                 tabProfileTween.ResetTween();
 
@@ -73,31 +69,9 @@ public class HeadquartersUI : MonoBehaviour
             profileCanvas.sortingOrder = 0;
             inventoryCanvas.sortingOrder = 0;
             storeCanvas.sortingOrder = 1;
-            playCanvas.sortingOrder = 0;
             tabStoreTween.Tween(()=> {
-                tabPlayTween.ResetTween();
                 tabInventoryTween.ResetTween();
                 tabProfileTween.ResetTween();
-
-                _isTransistioning = false;
-
-            });
-        });
-
-        tabPlayButton.onClick.AddListener(() => {
-
-            if (_isTransistioning) return;
-
-            _isTransistioning = true;
-
-            profileCanvas.sortingOrder = 0;
-            inventoryCanvas.sortingOrder = 0;
-            storeCanvas.sortingOrder = 0;
-            playCanvas.sortingOrder = 1;
-            tabPlayTween.Tween(()=> {
-                tabInventoryTween.ResetTween();
-                tabProfileTween.ResetTween();
-                tabStoreTween.ResetTween();
 
                 _isTransistioning = false;
 
